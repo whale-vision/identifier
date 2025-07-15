e2cb289a76ba9c29b1170694306b578268d8787b
Sammie <Alphasam123@gmail.com>
2025-07-15 13:30:46 +0100

    idk whats going on anymore

--- src/websocket.py
+++ src/websocket.py
@@ -1,40 +1,52 @@
 import asyncio
-from featureExtraction.featureExtraction import getDevice, getPath
 import websockets
 import json
 from fileHandler import saveWhaleIdentities 
 from main import extractWhales, identifyWhales
 
-async def handle(websocket):
-    async for message in websocket:
-        try: 
-            content = json.loads(message)
-            print(f"received: {content["type"]}")
+from featureExtraction.featureExtraction import extractor, getDevice, getPath
+from identification.identification import identifier
+from segmentation.segmentation import segmentor
 
-            if (content["type"] == "extract"):
-                await websocket.send(f"starting on {getDevice()} with {getPath()}")
+def curriedHandle(imageSegmentor, imageExtractor, imageIdentifier):
+    async def handle(websocket):
+        async for message in websocket:
+            try: 
+                content = json.loads(message)
+                print(f"received: {content["type"]}")
+
+                if (content["type"] == "extract"):
+                    await websocket.send(f"starting on {getDevice()} with {getPath()}")
+                    
+                    fileNames = content["fileNames"]
+                    await extractWhales(fileNames, websocket, imageSegmentor, imageExtractor)
+
+                elif (content["type"] == "identify"):
+                    await websocket.send(f"starting on {getDevice()} with {getPath()}")
+
+                    data = content["data"]
+                    await identifyWhales(data, websocket, imageIdentifier)
+
+                elif (content["type"] == "save"):
+                    whaleIdentities = content["whaleIdentities"]
+                    await saveWhaleIdentities(whaleIdentities, websocket)
                 
-                fileNames = content["fileNames"]
-                await extractWhales(fileNames, websocket)
 
-            elif (content["type"] == "identify"):
-                await websocket.send(f"starting on {getDevice()} with {getPath()}")
+                await websocket.send(f"complete!")
+            except Exception as e:
+                print(e)
+                await websocket.send(f"error: {e}")
 
-                data = content["data"]
-                await identifyWhales(data, websocket)
-
-            elif (content["type"] == "save"):
-                whaleIdentities = content["whaleIdentities"]
-                await saveWhaleIdentities(whaleIdentities, websocket)
-            
-
-            await websocket.send(f"complete!")
-        except Exception as e:
-            print(e)
-            await websocket.send(f"error: {e}")
+    return handle
 
 async def main():
-    async with websockets.serve(handle, "localhost", 8765):
+    imageSegmentor = segmentor()
+    imageExtractor = extractor()
+    imageIdentifier = identifier()
+
+    print(f"Starting WebSocket server on ws://localhost:8765")
+    
+    async with websockets.serve(curriedHandle(imageSegmentor, imageExtractor, imageIdentifier), "localhost", 8765):
         await asyncio.Future()  # Run forever
 
 if __name__ == "__main__":
